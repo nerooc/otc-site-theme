9a9842ee95e845d9dd56fec5d9b154950dcc11d9
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-04-06 22:32:52 +0200

    back to top button

--- src/js/products.js
+++ src/js/products.js
@@ -20,3 +20,24 @@ option3.addEventListener("click", function () {
     option2.classList.remove('active');
     option3.classList.add('active');
 });
+
+//BACK2TOP BUTTON
+b2t = document.getElementById("b2t");
+
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        b2t.style.opacity = 1;
+    } else {
+        b2t.style.opacity = 0;
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
